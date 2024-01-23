28c3b64d3ddca6433c9538f235086e9b64924640
sifeddineelbakhti <157433673+sifeddineelbakhti@users.noreply.github.com>
2024-01-23 14:36:48 +0100

    Update ✅ Joined.js

--- commands/✅ Joined.js
+++ commands/✅ Joined.js
@@ -22,7 +22,7 @@ if (stat=="ban"){
 Bot.sendMessage("*You're Ban From Using The Bot ❌*");
 }else{
 
-let channel = "@cheapestsmmpro";
+let channel = "@marocsmm";
 let id = user.telegramid
 Api.getChatMember({ 
 chat_id : channel,
